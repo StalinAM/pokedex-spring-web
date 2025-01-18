341ff4defa970640e344d57c2eb350a281ad0eff
Angelo Silva <amsilvac1@uce.edu.ec>
2025-01-18 10:36:43 -0500

    add pokeAPI and pokeservice

--- src/main/java/ec/edu/uce/pokedex/PokedexApplication.java
+++ src/main/java/ec/edu/uce/pokedex/PokedexApplication.java
@@ -1,5 +1,6 @@
 package ec.edu.uce.pokedex;
 
+import ec.edu.uce.pokedex.api.PokeService;
 import ec.edu.uce.pokedex.entities.Pokemon;
 import ec.edu.uce.pokedex.service.PokemonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,24 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PokedexApplication implements CommandLineRunner {
 
     @Autowired
-    private PokemonService pokemonService;
+    private PokeService pokeService;
 
     public static void main(String[] args) {
-        SpringApplication.run(PokedexApplication.class, args);
+        SpringApplication.run(PokedexApplication.class, args); }
 
-
-    }
     @Override
     public void run(String... args) throws Exception {
-        Pokemon pokemon = new Pokemon();
-        pokemon.setName("Pikachu");
-        pokemon.setWeight(6.0);
-        pokemon.setHeight(0.4);
-
-        pokemonService.savePokemon(pokemon);
-
-        System.out.println("Pokemon guardado: " + pokemon.getName());
-    }
+        pokeService.fetchAndSaveAllPokemon(); }
 
 
 
---
+++ src/main/java/ec/edu/uce/pokedex/api/PokeService.java
@@ -0,0 +1,79 @@
+package ec.edu.uce.pokedex.api;
+
+import ec.edu.uce.pokedex.entities.Abilities;
+import ec.edu.uce.pokedex.entities.Pokemon;
+import ec.edu.uce.pokedex.entities.Type;
+import ec.edu.uce.pokedex.repository.PokemonRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PokeService {
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    public void fetchAndSaveAllPokemon() throws Exception {
+        PokeAPI apiClient = new PokeAPI();
+        List<String> pokemonUrls = apiClient.fetchAllPokemonUrls();
+
+        for (String url : pokemonUrls) {
+            // Realiza la solicitud para cada Pokémon
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            JSONObject pokemonJson = new JSONObject(response.toString());
+
+            Pokemon pokemon = new Pokemon();
+            pokemon.setName(pokemonJson.getString("name"));
+            pokemon.setHeight(pokemonJson.getDouble("height"));
+            pokemon.setWeight(pokemonJson.getDouble("weight"));
+
+            // Manejar potencial null en 'front_default'
+            JSONObject sprites = pokemonJson.getJSONObject("sprites");
+            String frontDefault = sprites.isNull("front_default") ? null : sprites.getString("front_default");
+            pokemon.setImage(frontDefault);
+
+            // Procesar habilidades
+            JSONArray abilitiesArray = pokemonJson.getJSONArray("abilities");
+            List<Abilities> abilities = new ArrayList<>();
+            for (int i = 0; i < abilitiesArray.length(); i++) {
+                String abilityName = abilitiesArray.getJSONObject(i).getJSONObject("ability").getString("name");
+                Abilities ability = new Abilities();
+                ability.setName(abilityName);
+                abilities.add(ability);
+            }
+            pokemon.setAbilities(abilities);
+
+            // Procesar tipos
+            JSONArray typesArray = pokemonJson.getJSONArray("types");
+            List<Type> types = new ArrayList<>();
+            for (int i = 0; i < typesArray.length(); i++) {
+                String typeName = typesArray.getJSONObject(i).getJSONObject("type").getString("name");
+                Type type = new Type();
+                type.setName(typeName);
+                types.add(type);
+            }
+            pokemon.setTypes(types);
+
+            // Guardar en la base de datos
+            pokemonRepository.save(pokemon);
+        }
+    }
+}
