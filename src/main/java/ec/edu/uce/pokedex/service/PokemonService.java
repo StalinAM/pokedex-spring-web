e39a26d9e633a021292a1271ffcb7f45f569d592
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-25 02:10:52 -0500

    add debug logging in getPokemonsByAbility method

--- src/main/java/ec/edu/uce/pokedex/service/PokemonService.java
+++ src/main/java/ec/edu/uce/pokedex/service/PokemonService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -44,6 +45,7 @@ public class PokemonService {
     // Buscar Pokémon por habilidad
     @Transactional(readOnly = true)
     public List<Pokemon> getPokemonsByAbility(String abilityName) {
+        System.out.println("abilityName: " + abilityName);
         return pokemonRepository.findByAbilities_Name(abilityName).stream()
                 .filter(pokemon -> pokemon.getAbilities().stream()
                         .anyMatch(ability -> ability.getName().equalsIgnoreCase(abilityName)))
