dbf846cc5bd0877d6c9bffea782174c36d032716
Stalin Acurio <sta_vla411@hotmail.com>
2025-02-09 16:49:32 -0500

    refactor PokedexApplication: remove unused PokemonService and simplify run method

--- src/main/java/ec/edu/uce/pokedex/PokedexApplication.java
+++ src/main/java/ec/edu/uce/pokedex/PokedexApplication.java
@@ -17,24 +17,13 @@ public class PokedexApplication implements CommandLineRunner {
     @Autowired
     private PokeService pokeService;
 
-    @Autowired
-    private PokemonService pokemonService;
     public static void main(String[] args) {
-        System.setProperty("java.awt.headless", "false");
-        SpringApplication.run(PokedexApplication.class, args); }
+        SpringApplication.run(PokedexApplication.class, args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
-        double timeData = pokeService.fetchAndSaveAllPokemon();
-        if (!GraphicsEnvironment.isHeadless()) {
-            SwingUtilities.invokeLater(() -> {
-                PokedexGUI pokedexGUI = new PokedexGUI(pokemonService, timeData);
-                pokedexGUI.setVisible(true);
-                pokedexGUI.loadPokemons();
-            });
-        } else {
-            System.out.println("Entorno sin GUI detectado. Ejecución en modo headless.");
-        }
+        pokeService.fetchAndSaveAllPokemon();
     }
 
 }
